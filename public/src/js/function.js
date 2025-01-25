d587ca0ee9f634f651df8df3df265b5193fa2c75
ikh05 <mi3061923002@gmail.com>
2025-01-26 07:33:10 +0800

    hilangkan fitur kembali

--- public/src/js/function.js
+++ public/src/js/function.js
@@ -228,6 +228,10 @@ document.addEventListener('DOMContentLoaded', function(e){
         })
     });
 })
+history.pushState(null, null, location.href);
+window.onpopstate = function () {
+    history.go(1);
+};
 
 
 
